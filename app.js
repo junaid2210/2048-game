dfe1f2a33dec8755d457d3d9b6edc624ae0be4dd
Junaid Khan <jk7466809@gmail.com>
2025-07-12 12:32:21 +0530

    added game over and restart button

--- app.js
+++ app.js
@@ -18,12 +18,28 @@ let highScore = localStorage.getItem("highScore") || 0;
 function updateScore() {
     document.getElementById("Score").innerText = score;
 
-    if(score > highScore){
+    if (score > highScore) {
         highScore = score;
-        localStorage.setItem("highScore" , highScore);
+        localStorage.setItem("highScore", highScore);
     }
 
     document.getElementById("high-score").innerText = highScore;
+}
+
+function checkGameOver() {
+    for (let r = 0; r < 4; r++) {
+        for (let c = 0; c < 4; c++) {
+            if (board[r][c] === 0)
+                return false;
+
+            if (c < 3 && board[r][c] === board[r][c + 1])
+                return false;
+
+            if (r < 3 && board[r][c] === board[r + 1][c])
+                return false;
+        }
+    }
+    return true;
 }
 
 function setUpBoard() {
@@ -90,6 +106,10 @@ function moveLeft() {
         }
 
         board[r] = row;
+        if (checkGameOver()) {
+            alert("Game Over! Try again.");
+        }
+
     }
 
     const newBoard = JSON.stringify(board);
@@ -117,7 +137,7 @@ function moveRight() {
             }
         }
 
-        row = row.filter(val => val !== 0);
+        row = board[r].filter(val => val !== 0);
 
         while (row.length < 4) {
             row.push(0);
@@ -133,6 +153,9 @@ function moveRight() {
 
     setUpBoard();
     updateScore();
+    if (checkGameOver()) {
+        alert("Game Over! Try again.");
+    }
 }
 
 function moveUp() {
@@ -142,36 +165,39 @@ function moveUp() {
         let col = [];
 
         for (let r = 0; r < 4; r++) {
-            
+
             if (board[r][c] !== 0) {
                 col.push(board[r][c]);
             }
         }
 
-        for(let i = 0 ; i < col.length - 1 ; i++){
-            if(col[i] === col[i+1]){
+        for (let i = 0; i < col.length - 1; i++) {
+            if (col[i] === col[i + 1]) {
                 col[i] *= 2;
                 score += col[i];
-                col[i+1] = 0;
+                col[i + 1] = 0;
             }
         }
 
         col = col.filter(val => val !== 0);
 
-        while(col.length < 4){
+        while (col.length < 4) {
             col.push(0);
         }
 
-        for(let r = 0 ; r < 4 ; r++){
+        for (let r = 0; r < 4; r++) {
             board[r][c] = col[r];
         }
     }
     const newBoard = JSON.stringify(board);
-    if(oldBoard != newBoard){
+    if (oldBoard != newBoard) {
         generateRandomTile();
     }
     setUpBoard();
     updateScore();
+    if (checkGameOver()) {
+        alert("Game Over! Try again.");
+    }
 }
 
 function moveDown() {
@@ -181,36 +207,55 @@ function moveDown() {
         let col = [];
 
         for (let r = 3; r >= 0; r--) {
-            
+
             if (board[r][c] !== 0) {
                 col.push(board[r][c]);
             }
         }
 
-        for(let i = 0 ; i < col.length - 1 ; i++){
-            if(col[i] === col[i+1]){
+        for (let i = 0; i < col.length - 1; i++) {
+            if (col[i] === col[i + 1]) {
                 col[i] *= 2;
                 score += col[i];
-                col[i+1] = 0;
+                col[i + 1] = 0;
             }
         }
 
         col = col.filter(val => val !== 0);
 
-        while(col.length < 4){
+        while (col.length < 4) {
             col.push(0);
         }
 
         col.reverse();
 
-        for(let r = 0 ; r < 4 ; r++){
+        for (let r = 0; r < 4; r++) {
             board[r][c] = col[r];
         }
     }
     const newBoard = JSON.stringify(board);
-    if(oldBoard != newBoard){
+    if (oldBoard != newBoard) {
         generateRandomTile();
     }
     setUpBoard();
     updateScore();
+    if (checkGameOver()) {
+        alert("Game Over! Try again.");
+    }
+
+    document.getElementById('restart-btn').addEventListener('click',resetGame)
+
+    function resetGame(){
+        for(let r = 0 ; r < 4 ; r++){
+            for(let c = 0 ; c < 4 ; c++){
+                board[r][c] = 0;
+            }
+        }
+
+        generateRandomTile();
+        generateRandomTile();
+
+        setUpBoard();
+        updateScore();
+    }
 }
